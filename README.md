df4164ac5a643c290ce0305b0d928ea134b35fce
alekseychernykh <alexeyshockman@gmail.com>
2022-02-22 18:45:20 +0300

    yep!

---
+++ README.md
@@ -0,0 +1,3 @@
+сборка: https://github.com/dDenysS/gulp-template
+
+pages: https://alexeyshockman.github.io/flex-grid/build/index.html
--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,53 @@ import $ from '../local_modules/jquery/dist/jquery.min'
 $(document).ready(() => {
     // eslint-disable-next-line no-console
     console.log(`document ready`)
-    let $flex = document.querySelector('.flex');
-    let $grid = document.querySelector('.grid');
-    let $flexBtn = document.querySelector('.flex-btn');
-    let $gridBtn = document.querySelector('.grid-btn');
+    const $flex = document.querySelector('.flex');
+    const $grid = document.querySelector('.grid');
+    const $flexBtn = document.querySelector('.flex-btn');
+    const $gridBtn = document.querySelector('.grid-btn');
+    const $contentBtn = document.querySelector('.content-btn')
 
-    $flexBtn.addEventListener('click', function (){
+    $flexBtn.addEventListener('click', function(event) {
+        console.log('flex listnr')
         $grid.style.display = 'none';
         $flex.style.display = 'block';
+        changeActiveBtn(event)
     })
 
-    $gridBtn.addEventListener('click', function (){
+    $gridBtn.addEventListener('click', function(event) {
+        console.log('grid listnr')
         $flex.style.display = 'none';
         $grid.style.display = 'block';
+        changeActiveBtn(event)
     })
+
+    $contentBtn.addEventListener('click', changeCardContet);
+
+    window.addEventListener('resize', changeCardContet);
+
+    function changeActiveBtn(e) {
+        let $allBtns = document.querySelectorAll('.stile-switcher > button');
+        $allBtns.forEach(e => e.classList.remove('active'));
+        $allBtns.forEach(e => e.classList.add('disable'));
+        e.currentTarget.classList.add('active')
+        e.currentTarget.classList.remove('disable')
+    }
+
+
+    function changeCardContet () {
+        const $cards = document.querySelectorAll('.card');
+        $cards.forEach(function (card) {
+            const $cardText = card.querySelector('.card-main-text');
+            const $cardTitle = card.querySelector('.card-main-title');
+            const $cardHref = card.querySelector('a')
+            const w = card.offsetWidth;
+            if (w < 280) {
+                $cardText.innerHTML = '<p style="color: red; font-weight: 900; font-size: 32px;">alert!</p>';
+            } else {
+                $cardText.innerText = `width ${w}px`;
+            }
+            $cardTitle.innerText = card.classList[1];
+            $cardHref.innerText = 'link';
+        })
+    }
 })
